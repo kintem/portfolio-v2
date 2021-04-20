ab851649f83fdc875431956c36796ea0bc3f6c0e
Kinte Matulyte <=>
2021-04-20 21:54:28 +0100

    navbar links and basic styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "@reach/router";
 import styles from "./NavBar.module.scss";
 
 const NavBar = () => {
   return (
-    <>
-      <p>NavBar works</p>
-    </>
+    <nav className={styles.navBar}>
+      <Link to="projects">Projects</Link>
+      <Link to="about">About</Link>
+      <Link to="contact">Contact</Link>
+    </nav>
   );
 };
 

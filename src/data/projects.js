3d79392c324266e340653d9a8a2ee807e45d6971
Kinte Matulyte <kintem@live.co.uk>
2021-05-05 20:33:39 +0100

    add project links

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,27 +5,42 @@ import morse from '../images/morse.png';
 const projects = [
   {
     name: 'College Thrifts',
-    img: sallysays1,
+    img: '',
     icons: ['fa-brands:react', 'bx-bxl-sass'],
-    description: 'MVP built for a client in 4 weeks during the _nology bootcamp'
+    description: 'MVP built for a client in 4 weeks during the _nology bootcamp',
+    githubURL: '',
+    liveURL: 'https://college-thrifts.web.app/',
   },
   {
     name: 'Sally Says',
-    img: sallysays2,
+    img: sallysays1,
     icons: ['fa-html5', 'fa-brands:js-square', 'bx-bxl-sass'],
-    description: 'JavaScript memory game based on Simon Says'
+    description: 'JavaScript memory game based on Simon Says',
+    githubURL: 'https://github.com/kintem/sally-says',
+    liveURL: 'https://kintem.github.io/sally-says/',
   },
   {
     name: 'Punk API App',
-    img: morse,
+    img: '',
     icons: ['fa-brands:react', 'bx-bxl-sass'],
-    description: 'React app built for Punk API'
+    description: 'A front-end for Punk API built using React',
+    githubURL: 'https://github.com/kintem/punkapi-app',
+    liveURL: '',
   },
   {
     name: 'Morse Code Translator',
     img: morse,
     icons: ['fa-html5', 'fa-brands:js-square', 'fa-brands:css3-alt'],
-    description: 'Simple Morse code translator built in 2.5 hours for one of the Tech Test challenges during the _nology bootcamp'
+    description: 'Simple Morse code translator built in 2.5 hours for one of the Tech Test challenges during the _nology bootcamp',
+    githubURL: 'https://github.com/kintem/morse-code-translator',
+    liveURL: 'https://kintem.github.io/morse-code-translator/',
+  },{
+    name: 'Ticket Tracker',
+    img: '',
+    icons: ['fa-brands:react', 'bx-bxl-sass'],
+    description: 'Ticket tracker app built using React and hosted with Firebase',
+    githubURL: 'https://github.com/kintem/ticket-tracker',
+    liveURL: 'https://ticket-tracker-9fe82.web.app/',
   },
 ]
 

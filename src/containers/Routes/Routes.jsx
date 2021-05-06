84c9dc38433e0902c4ed9a392aa126af5e017e55
Kinte Matulyte <kintem@live.co.uk>
2021-05-06 22:08:23 +0100

    add contact icons with links

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import { Router } from '@reach/router';
 import Home from '../Home';
 import About from '../About';
 import Projects from '../Projects';
+import Contact from '../Contact';
 
 const Routes = (props) => {
   const { setActivePage } = props;
@@ -12,6 +13,7 @@ const Routes = (props) => {
       <Home path='/portfolio-v2' setActivePage={setActivePage} />
       <About path='about' setActivePage={setActivePage} />
       <Projects path='projects' setActivePage={setActivePage} />
+      <Contact path='contact' setActivePage={setActivePage} />
     </Router>
   )
 };
